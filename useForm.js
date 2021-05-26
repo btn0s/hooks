4c8d2f254dbbf1b69b3f718416cac0f245f69478
Brendan Norris <btn0s@users.noreply.github.com>
2021-05-25 17:32:55 -0700

    added description to useForm

--- useForm.js
+++ useForm.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
 
+/**
+ * Description: A simple hook for magically managing state of a form.
+ * Usage: Attach the returned "handleFormChange" handler to your <form> element, and add an "id" attribute to any form element. 
+ * This id will be used as the key in the form state, if you do not wish for a given element to be saved to form state the id should be 'no-save'.
+ */
+
 export const useForm = () => {
   const [formValues, setFormValues] = useState();
   const handleFormChange = (e) => {
@@ -12,3 +18,4 @@ export const useForm = () => {
   };
   return [formValues, handleFormChange];
 };
+
